cbff8a213ad54f6aedcd5a4eb0ff6cdb4036a1b9
Riku-1 <osaka.riku@gmail.com>
2020-07-30 15:48:36 +0900

    Change field name to more clearly name

--- blog/repositories/UserFactory.py
+++ blog/repositories/UserFactory.py
@@ -10,17 +10,17 @@ from blog.models import UserModel
 
 class IUserFactory(ABC):
     @abstractmethod
-    def get(self, user: UserModel) -> IUser:
+    def get(self, user_model: UserModel) -> IUser:
         pass
 
 
 class UserFactory(IUserFactory):
-    def get(self, user: UserModel) -> IUser:
-        user_id = user.pk
-        first_name = user.first_name
-        last_name = user.last_name
-        username = user.username
-        grade = user.grade
+    def get(self, user_model: UserModel) -> IUser:
+        user_id = user_model.pk
+        first_name = user_model.first_name
+        last_name = user_model.last_name
+        username = user_model.username
+        grade = user_model.grade
 
         if grade.pk == GradeValue.FREE.value:
             return FreeUser(user_id, first_name, last_name, username)
