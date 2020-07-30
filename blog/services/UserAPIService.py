4b9c0667f869224f247dbd78a47840be21d5b81a
Riku-1 <osaka.riku@gmail.com>
2020-07-31 01:19:08 +0900

    Fix type is not correct

--- blog/services/UserAPIService.py
+++ blog/services/UserAPIService.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
 
+from django.http import JsonResponse
+
 from blog.repositories.UserRepository import IUserRepository
 from blog.response_creators.UserResponseCreator import IUserResponseCreator
 
 
 class IUserAPIService(ABC):
     @abstractmethod
-    def get(self, user_id: int) -> dict:
+    def get(self, user_id: int) -> JsonResponse:
         pass
 
 
@@ -19,6 +21,6 @@ class UserAPIService(IUserAPIService):
         self.repository = IoCContainer.user_repository()
         self.json_creator = IoCContainer.user_api_json_creator()
 
-    def get(self, user_id: int):
+    def get(self, user_id: int) -> JsonResponse:
         user = self.repository.get(user_id)
         return self.json_creator.get(user)
