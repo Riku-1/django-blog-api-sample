ec0a01100a24ed6135d820509f917a1ebb61ecf2
Riku-1 <osaka.riku@gmail.com>
2020-07-30 10:23:47 +0900

    Fix setUpTestData method was not working

--- blog/tests/unit/repositories/UserRepositoryTest.py
+++ blog/tests/unit/repositories/UserRepositoryTest.py
@@ -11,13 +11,12 @@ from blog.repositories.UserRepository import UserRepository
 first_name = "test__first"
 last_name = "test__last"
 grade = Mock(value=1)
-user_id = 99999999
 
 
 # This class does not test database. Test with db is executed in integration tests.
 class UserRepositoryTest(TestCase):
     @classmethod
-    def setUpTestData(cls):
+    def setUpClass(cls):
         auth_user: User = User()
         auth_user.first_name = first_name
         auth_user.last_name = last_name
@@ -25,14 +24,11 @@ class UserRepositoryTest(TestCase):
         auth_user.save()
 
         user: UserModel = UserModel()
-        user.pk = user_id
         user.auth_user = auth_user
         user.grade = GradeModel.objects.get(pk=grade.value)
         user.save()
+        cls.user_id = user.id
 
-    @classmethod
-    def setUpClass(cls):
-        user = Mock(first_name=first_name, last_name=last_name, grade=grade)
         factory = Mock(get=MagicMock(return_value=user))
         IoCContainer.user_factory.override(factory)
 
@@ -41,7 +37,7 @@ class UserRepositoryTest(TestCase):
         IoCContainer.user_factory.reset_override()
 
     def test_get(self):
-        actual = UserRepository().get(user_id)
+        actual = UserRepository().get(self.user_id)
 
         self.assertEqual(first_name, actual.first_name)
         self.assertEqual(last_name, actual.last_name)
