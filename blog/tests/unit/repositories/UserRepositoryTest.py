8609d52a6fd5c4a7351516d91e341a7fc9ae393e
Riku-1 <osaka.riku@gmail.com>
2020-07-30 11:52:19 +0900

    Modefied test to conform to the change in UserModel

--- blog/tests/unit/repositories/UserRepositoryTest.py
+++ blog/tests/unit/repositories/UserRepositoryTest.py
@@ -1,5 +1,6 @@
 from unittest.mock import Mock, MagicMock
 
+from django.db import IntegrityError
 from django.test import TestCase
 
 from blog.IoCContainer import IoCContainer
@@ -10,7 +11,7 @@ from blog.repositories.UserRepository import UserRepository
 first_name = "test__first"
 last_name = "test__last"
 nick_name = "test_nickname"
-grade = Mock(value=1)
+grade_id = 1
 
 
 # This class does not test database. Test with db is executed in integration tests.
@@ -21,7 +22,7 @@ class UserRepositoryTest(TestCase):
         user.first_name = first_name
         user.last_name = last_name
         user.username = nick_name
-        user.grade = GradeModel.objects.get(pk=grade.value)
+        user.grade = GradeModel.objects.get(pk=grade_id)
         user.save()
         cls.user_id = user.id
 
@@ -37,12 +38,25 @@ class UserRepositoryTest(TestCase):
 
         self.assertEqual(first_name, actual.first_name)
         self.assertEqual(last_name, actual.last_name)
-        self.assertEqual(grade.value, actual.grade.value)
+        self.assertEqual(grade_id, actual.grade.value)
         self.assertEqual(nick_name, actual.nick_name)
 
     def test_save(self):
-        actual = UserRepository().save(first_name, last_name, grade)
+        nick_name_save = "test_save"
+        grade = Mock(
+            value=1
+        )
+
+        actual = UserRepository().save(first_name, last_name, nick_name_save, grade)
 
         self.assertEqual(first_name, actual.first_name)
         self.assertEqual(last_name, actual.last_name)
-        self.assertEqual(grade, actual.grade)
+        self.assertEqual(grade_id, actual.grade.value)
+
+    def test_save_duplicate(self):
+        with self.assertRaises(
+            IntegrityError
+        ):
+            grade_value = Mock(value=grade_id)
+            UserRepository().save(first_name, last_name, nick_name, grade_value)
+
